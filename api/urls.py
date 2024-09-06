363b2d2dca96eb11c17935abcf84a48176d1e0f9
= <sohailahmed34280@gmail.com>
2024-09-06 12:15:51 +0500

    Test Api endpoints using browserable api

--- api/urls.py
+++ api/urls.py
@@ -1,12 +1,10 @@
-from django.contrib import admin
 from django.urls import path
 from api import views
 
 urlpatterns = [
-         path('studentapilist/', views.StudentList.as_view()),
-         path('studentapicreate/', views.StudentCreate.as_view()),
-         path('studentapiretreive/<int:pk>/', views.StudentRetreive.as_view()),
-         path('studentapiupdate/<int:pk>/', views.StudentUpdate.as_view()),
-         path('studentapidestroy/<int:pk>/', views.StudentDestroy.as_view()),
-
+    path('studentapilist/', views.StudentList.as_view(), name='student-list'),
+    path('studentapicreate/', views.StudentCreate.as_view(), name='student-create'),
+    path('studentapiretreive/<int:pk>/', views.StudentRetreive.as_view(), name='student-retrieve'),
+    path('studentapiupdate/<int:pk>/', views.StudentUpdate.as_view(), name='student-update'),
+    path('studentapidestroy/<int:pk>/', views.StudentDestroy.as_view(), name='student-destroy'),
 ]
